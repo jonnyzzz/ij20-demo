0e488b8105c7bdba16dde93ec491a3b37a25afe2
Eugene Petrenko <me@jonnyzzz.com>
2021-02-25 23:10:40 +0100

    format code

--- src/ReadAllFiles.kt
+++ src/ReadAllFiles.kt
@@ -1,23 +1,18 @@
-import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.stream.Collectors
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    val home = Path.of("/Users/jonnyzzz/Work/intellij-community")
+    val ijCommunityHome = Path.of("/Users/jonnyzzz/Work/intellij-community")
 
     repeat(5) {
         val time = measureTimeMillis {
-
-            Files.walk(home)
+            Files.walk(ijCommunityHome)
                 .parallel()
                 .filter { Files.isRegularFile(it) }
                 .forEach { runCatching { Files.readAllBytes(it) } }
         }
 
-            println("Java + Kotlin read is completed in: ${time}ms")
-        }
+        println("The read is completed in: ${time}ms")
     }
-
-    private fun String.endsWithAny(vararg suffix: String) = suffix.any { endsWith(it) }
+}
